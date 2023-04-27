4876a9bcc34c4913d2beb2225d371d5f0bc06ed9
Pranav Patil <121039421+pranavpatil15198@users.noreply.github.com>
2023-04-27 16:00:07 +0530

    Update OrderRepository.java

--- src/main/java/com/driver/OrderRepository.java
+++ src/main/java/com/driver/OrderRepository.java
@@ -1,25 +1,39 @@
 package com.driver;
 
-import lombok.Data;
-import org.springframework.stereotype.Repository;
+import java.util.Arrays;
+import java.util.List;
 
-import java.util.HashMap;
+public class Order {
 
-@Data
-@Repository
-public class OrderRepository {
-    private HashMap<String, Order> orders = new HashMap<>();
+    private String id;
+    private int deliveryTime;
 
-    public Order addOrder(Order order){
-        orders.put(order.getId(), order);
-        return order;
+    public Order(String id, String deliveryTime) {
+
+        // The deliveryTime has to converted from string to int and then stored in the attribute
+        int HH = Integer.parseInt(deliveryTime.substring(0,2));
+        int MM = Integer.parseInt(deliveryTime.substring(3));
+
+        this.deliveryTime  = HH*60 + MM;
     }
 
-    public Order getOrderById(String orderId){
-        return orders.getOrDefault(orderId, new Order());
+    public void setId(String id) {
+        this.id = id;
     }
 
-    public void deleteOrder(String orderId){
-        orders.remove(orderId);
+    public void setDeliveryTime(int deliveryTime) {
+        this.deliveryTime = deliveryTime;
     }
+
+    public static int getDeliveryTimeInt(String deliveryTime) {
+        List<String> l = Arrays.asList(deliveryTime.split(":"));
+        int totalTime = (Integer.parseInt(l.get(0)) *60) + Integer.parseInt(l.get(1));
+        return totalTime;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getDeliveryTime() {return deliveryTime;}
 }
