6a42502381aef508a98c661aea099ebc085d4d77
Pranav Patil <121039421+pranavpatil15198@users.noreply.github.com>
2023-04-27 15:05:50 +0530

    Update Order.java

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -5,10 +5,20 @@ public class Order {
     private String id;
     private int deliveryTime;
 
+    public Order(){};
+
     public Order(String id, String deliveryTime) {
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
+        int time = 0;
+        String hrs = deliveryTime.substring(0, 2);
+        String mins = deliveryTime.substring(3);
+        time += Integer.parseInt(hrs)*60 + Integer.parseInt(mins);
+
+        this.id = id;
+        this.deliveryTime = time;
+
     }
 
     public String getId() {
