66837a1d66f8be933eafebaa9154d6defda16677
Pranav Patil <121039421+pranavpatil15198@users.noreply.github.com>
2023-05-28 10:24:21 +0530

    done

--- src/main/java/com/driver/DeliveryPartner.java
+++ src/main/java/com/driver/DeliveryPartner.java
@@ -1,53 +1,29 @@
+package com.driver;
+
 public class DeliveryPartner {
-    private String partnerId;
-    private List<Order> orders;
 
-    // Constructors, getters, and setters
+    private String id;
+    private int numberOfOrders;
 
-    public DeliveryPartner(String partnerId) {
-        this.partnerId = partnerId;
-        this.orders = new ArrayList<>();
+    public DeliveryPartner(String id) {
+        this.id = id;
+        this.numberOfOrders = 0;
     }
 
-    public String getPartnerId() {
-        return partnerId;
+
+    public void setId(String id) {
+        this.id = id;
     }
 
-    public void setPartnerId(String partnerId) {
-        this.partnerId = partnerId;
+    public String getId() {
+        return id;
     }
 
-    public List<Order> getOrders() {
-        return orders;
+    public Integer getNumberOfOrders(){
+        return numberOfOrders;
     }
 
-    public void setOrders(List<Order> orders) {
-        this.orders = orders;
-    }
-
-    public void addOrder(Order order) {
-        orders.add(order);
-    }
-
-    public int getOrderCount() {
-        return orders.size();
-    }
-
-    public int getOrdersLeftAfterGivenTime(String time) {
-        int count = 0;
-        for (Order order : orders) {
-            if (order.getDeliveryTime().compareTo(time) > 0) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    public String getLastDeliveryTime() {
-        if (orders.isEmpty()) {
-            return "";
-        }
-        Order lastOrder = orders.get(orders.size() - 1);
-        return lastOrder.getDeliveryTime();
+    public void setNumberOfOrders(Integer numberOfOrders) {
+        this.numberOfOrders = numberOfOrders;
     }
 }
